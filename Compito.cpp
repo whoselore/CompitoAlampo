b265588aada5c3a683b280dc27488f81d4ef53f8
whoselore <44800409+whoselore@users.noreply.github.com>
2018-12-11 09:24:30 +0100

    Update Compito.cpp

--- Compito.cpp
+++ Compito.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 using namespace std;
 int main(){
+    <<<<<<< HEAD
+int a=0;
+=======
+int a=1;
+    >>>>>>> 
     for(int contatore=0;contatore<10;contatore++){
             contatore<<contatore+1;
     }
