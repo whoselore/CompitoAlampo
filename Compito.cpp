ecb7457bdd2f26f8f34fc3feb0deaa4d5fc3e986
whoselore <alampo.lorenzo@gmail.com>
2018-12-11 09:00:34 +0100

    Correzione finale

--- Compito.cpp
+++ Compito.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 int main(){
-    for(int contatore=0;contatore<10;i++){
+    for(int contatore=0;contatore<10;contatore++){
             contatore<<contatore+1;
     }
 }
